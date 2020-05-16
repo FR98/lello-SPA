481bcfb18dae1c9e5061c6fb775eccd7df4bc4da
FR98 <ros18676@uvg.edu.gt>
2020-05-16 15:27:41 -0600

    Structure

---
+++ lello/src/components/App/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { configureStore } from '../../store';
+
+import './styles.css';
+
+const store = configureStore();
+
+const App = () => (
+    <div className = "App">
+        <Provider store = {store}>
+            <Router>
+                <Switch>
+
+                    <Route path = '/'>
+                        {/* <Index/> */}
+                    </Route>
+                    <Route path = '/login'>
+                        {/* <Login/> */}
+                    </Route>
+                    <Route path='/signup'>
+                        {/* <SignUp/> */}
+                    </Route>
+
+                </Switch>
+            </Router>
+        </Provider>
+    </div>
+);
+
+export default App;
