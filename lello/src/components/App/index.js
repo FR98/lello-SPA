e18f8555cf0286afe4854df4fe62e14dd2639467
FR98 <ros18676@uvg.edu.gt>
2020-05-18 16:30:05 -0600

    Persistor

--- lello/src/components/App/index.js
+++ lello/src/components/App/index.js
@@ -2,28 +2,31 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { configureStore } from '../../store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './styles.css';
 
 import Index from '../Index';
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 const App = () => (
     <div className = "App">
         <Provider store = {store}>
-            <Router>
-                <Switch>
+            <PersistGate loading = { null } persistor = { persistor }>
+                <Router>
+                    <Switch>
 
-                    <Route path = '/'>
-                        <Index/>
-                    </Route>
-                    <Route path='/signup'>
-                        {/* <SignUp/> */}
-                    </Route>
+                        <Route path = '/'>
+                            <Index/>
+                        </Route>
+                        <Route path = '/signup'>
+                            {/* <SignUp/> */}
+                        </Route>
 
-                </Switch>
-            </Router>
+                    </Switch>
+                </Router>
+            </PersistGate>
         </Provider>
     </div>
 );
--- lello/src/store.js
+++ lello/src/store.js
@@ -1,5 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import {
+    persistStore,
+    persistReducer,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+// NATIVE: @react-native-community/async-storage
 
 import reducer from './reducers';
 import mainSaga from './sagas';
@@ -7,12 +13,24 @@ import mainSaga from './sagas';
 export const configureStore = () => {
     const sagaMiddleware = createSagaMiddleware();
 
-    const store = createStore(
+    const persistConfig = {
+        key: 'rootx',
+        storage,
+        whitelist: ['auth'],
+    };
+    
+    const persistedReducer = persistReducer(
+        persistConfig,
         reducer,
-        applyMiddleware(sagaMiddleware),
     );
 
+    const store = createStore(
+        persistedReducer,
+        applyMiddleware(sagaMiddleware),
+    );
+        
+    const persistor = persistStore(store);
     sagaMiddleware.run(mainSaga);
 
-    return store;
+    return { store, persistor };
 }
