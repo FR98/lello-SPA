87b7f5d88793bd5a57957ef4d961fa43e21eac34
Gian Luca Rivera <gian.luca.99@hotmail.com>
2020-05-30 18:16:30 -0600

    audits types

---
+++ lello/src/types/audits.js
@@ -0,0 +1,19 @@
+
+// state:
+// -lists:
+//     -byId
+//     -order
+//     -isFetching
+//     -error
+
+export const FETCH_AUDITS_STARTED = 'FETCH_AUDITS_STARTED';
+export const FETCH_AUDITS_COMPLETED = 'FETCH_AUDITS_COMPLETED';
+export const FETCH_AUDITS_FAILED = 'FETCH_AUDITS_FAILED';
+
+// export const ADD_AUDIT_STARTED = 'ADD_AUDIT_STARTED';
+// export const ADD_AUDIT_COMPLETED = 'ADD_AUDIT_COMPLETED';
+// export const ADD_AUDIT_FAILED = 'ADD_AUDIT_FAILED';
+
+// export const REMOVE_AUDIT_STARTED = 'REMOVE_AUDIT_STARTED';
+// export const REMOVE_AUDIT_COMPLETED = 'REMOVE_AUDIT_COMPLETED';
+// export const REMOVE_AUDIT_FAILED = 'REMOVE_AUDIT_FAILED';
